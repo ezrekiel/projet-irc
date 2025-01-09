82fc2ad959830c54eceba32803cd3fa33a5ece2f
ezrekiel <axel.theule@gmail.com>
2025-01-09 14:20:57 +0100

    initial

---
+++ back/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use((req, res, next) => {
+	bodyParser.json()(req, res, err => {
+		if (err) return res.status(400).send({ message: 'Error : Bad JSON formatting.' });
+
+		next();
+	});
+});
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur le serveur');
+});
+
+io.on('connection', (socket) => {
+    console.log('Un utilisateur s\'est connecté');
+
+    socket.on('disconnect', () => {
+        console.log('Un utilisateur s\'est déconnecté');
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Serveur lancé sur le port ${PORT}`);
+});
