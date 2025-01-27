f9b94ccbf3823e2c5b5778532f274a48ddeeb812
Brayan Jaozafy <brayan.jaozafy@epitech.eu>
2025-01-27 17:17:45 +0100

    css

--- front/src/components/Board.tsx
+++ front/src/components/Board.tsx
@@ -1,23 +1,26 @@
 import { useState } from "react";
 import Input from "./Input";
+import "./../index.css";
 
 const Board: React.FC = () => {
-    const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<string[]>([]);
 
-    const handleNewMessage = (messages: string) => {
-        setMessages((prevMessages) => [...prevMessages, messages]);
-    };
+  const handleNewMessage = (messages: string) => {
+    setMessages((prevMessages) => [...prevMessages, messages]);
+  };
 
-    return (
-        <>
-        <ul>
-            {messages.map((msg, index) => (
-                <li key={index}>{msg}</li>
-            ))}
-        </ul>
+  return (
+    <>
+      <ul>
+        {messages.map((msg, index) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul>
+      <div className="input">
         <Input onSubmit={handleNewMessage} />
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Board;
